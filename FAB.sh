78c9037b887793a017c9675cc83b8e93db13dedf
huxiaotian <huxiaotian@chunyu.me>
2020-01-07 19:03:54 +0800

    aa

--- FAB.sh
+++ FAB.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #!/bin/sh
 # 直接访问默认virtualenv中的fab
-if [ -f /home/chunyu/workspace/zongzong/ENV/bin/fab ]; then
-    /home/chunyu/workspace/ENV/bin/fab "$@"
+if [ -f /Users/huxiaotian/zongzong/ENV/bin/fab ]; then
+    /Users/huxiaotian/zongzong/ENV/bin/fab "$@"
 elif [ -f ~/workspace/zongzong/ENV/bin/fab ]; then
     ~/workspace/ENV/bin/fab "$@"
 else
