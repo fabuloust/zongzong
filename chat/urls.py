0b2b7f743963089efab2fa6c9f1e40d98db35538
huxiaotian <huxiaotian@chunyu.me>
2020-01-11 18:35:07 +0800

    temp

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,7 @@ from chat.views import get_my_conversation_list_view, post_content_view
 urlpatterns = [
     path('conversation_list/', get_my_conversation_list_view),
     path('post_content/', post_content_view),
-    path('get_activity_detail/', activity_detail_view),
+    # path('get_conversation_detail/', activity_detail_view),
     # path('participate_activity/', participate_activity_view),
 
 ]
