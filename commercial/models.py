5d406c06c9cda2fce807d478a17eed98dbaa33bb
huxiaotian <huxiaotian@chunyu.me>
2020-01-12 16:55:31 +0800

    更新数据库

--- commercial/models.py
+++ commercial/models.py
@@ -12,7 +12,7 @@ class Club(models.Model):
     name = models.CharField(max_length=40, verbose_name='俱乐部名称')
     fans_num = models.PositiveIntegerField(default=0, verbose_name='粉丝数量')
     address = models.CharField(max_length=100, verbose_name='俱乐部名称')
-    avatar = models.CharField(max_length=100, verbose_name='头像')
+    avatar = models.CharField(max_length=250, verbose_name='头像')
     telephone = models.CharField(max_length=15, verbose_name='头像')
     lat = models.FloatField(verbose_name='维度')
     lon = models.FloatField(verbose_name='经度')
@@ -35,7 +35,7 @@ class CommercialActivity(models.Model):
     detail = models.TextField(verbose_name='详细描述')
     total_quota = models.IntegerField(default=0, verbose_name='总名额')
     participant_num = models.IntegerField(default=0, verbose_name='报名名额')
-    top_image = models.CharField(max_length=200, verbose_name='顶部')
+    top_image = models.CharField(max_length=250, verbose_name='顶部')
     image_list = models.TextField(verbose_name='图片列表')
 
     created_time = models.DateTimeField(auto_now_add=True)
@@ -66,7 +66,7 @@ class TopBanner(models.Model):
     def __unicode__(self):
         return '{}'.format(self.title)
     title = models.CharField(max_length=40, verbose_name='标题')
-    image = models.CharField(max_length=40, verbose_name='图片')
+    image = models.TextField(verbose_name='图片')
     activity_id = models.PositiveIntegerField(verbose_name='活动id')
     is_online = models.BooleanField(default=False, verbose_name='是否上线')
     created_time = models.DateTimeField(auto_now_add=True)
