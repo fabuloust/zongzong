df297a721a28be9fafe8ac36b1b13980bb2f2993
huxiaotian <huxiaotian@chunyu.me>
2020-01-12 16:35:41 +0800

    add admin

---
+++ commercial/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from reversion.admin import VersionAdmin
+
+from commercial.models import Club, CommercialActivity, TopBanner
+
+
+class ClubAdmin(VersionAdmin):
+    list_display = ['name', 'address', 'telephone']
+
+
+admin.site.register(Club, ClubAdmin)
+
+
+class ActivityAdmin(VersionAdmin):
+    list_display = ['club', 'name', 'address']
+
+
+admin.site.register(CommercialActivity, ActivityAdmin)
+
+
+class TopBannerAdmin(VersionAdmin):
+    list_display = ['club', 'name', 'address']
+
+
+admin.site.register(TopBanner, TopBannerAdmin)
