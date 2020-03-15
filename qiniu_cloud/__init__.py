dc87658d137d12c8c0245aa9a266a2eceb4b5517
huxiaotian <huxiaotian@chunyu.me>
2020-03-16 00:06:32 +0800

    bug fix

--- qiniu_cloud/__init__.py
+++ qiniu_cloud/__init__.py
@@ -3,4 +3,4 @@
 # 服务器上传使用的bucket, 即 chunyu-files
 from qiniu_cloud.manager import QiniuCloudStorageManager
 
-ServerBucket = QiniuCloudStorageManager(bucket='chunyu-files', default_host='')
+ServerBucket = QiniuCloudStorageManager(bucket='zongz', default_host='')
