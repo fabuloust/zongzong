85e388f0f5d0af1090f41acbb0efc6d474a534dc
huxiaotian <huxiaotian@chunyu.me>
2020-03-15 13:50:52 +0800

    add debug

--- utilities/content_check.py
+++ utilities/content_check.py
@@ -24,7 +24,7 @@ def is_content_valid(content):
     headers = {'Content-Type': 'application/json'}
     result = requests.post(MSG_URL.format(access_token), data.encode('utf-8'), headers=headers).json()
     if result['errcode'] != 0:
-        print result
+        print(result)
         info_logger.info('content:{}, check result: {}'.format(content, result['error_msg']))
         return False
     return True
